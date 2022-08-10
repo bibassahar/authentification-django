5f9db94b4448768bb604f483be19ce024652222f
unknown <sahar.bibas@esen.tn>
2022-08-10 15:47:55 +0100

    messege5

--- authentification/authen/urls.py
+++ authentification/authen/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from authen import views
 
 urlpatterns=[
-    path('register/',views.RegisterView.as_view(),name='register'),
+    path('register',views.RegisterView.as_view(),name='register'),
     path('verify-email/',views.VerifyEmail.as_view(),name='verify-email')
 
 ]
