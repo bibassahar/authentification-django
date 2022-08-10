c6e72f06c7067ff16ccc0e2c331d9e23171bd2de
unknown <sahar.bibas@esen.tn>
2022-08-10 14:40:19 +0100

    commit 3

--- authentification/authen/views.py
+++ authentification/authen/views.py
@@ -1,7 +1,13 @@
+import email
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import generics,status
 from authen.serilaizers import RegisterSerilaizer
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utlis import util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 # Create your views here.
 class RegisterView(generics.GenericAPIView):
     authentication_classes = []
@@ -12,4 +18,18 @@ class RegisterView(generics.GenericAPIView):
         serilaizer.is_valid(raise_exception=True)
         serilaizer.save()
         user_data = serilaizer.data
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativelink = reverse('verify-email')
+        absurl = 'http/'+current_site +relativelink+'?token='+str(token)
+        email_body = 'Hi ' +user.username+ ' Use link below to verify your email \n '+absurl
+        data = {'email_body':email_body,'email_to':user.email,'email_subject': 'verify your email',}
+        util.send_email(data)
         return Response(user_data,status=status.HTTP_201_CREATED)
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
+
+
